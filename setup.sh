016d2fba290a617d6335182bae6b540bc30e1a59
thegreenhoneybee <25596930+thegreenhoneybee@users.noreply.github.com>
2023-07-23 18:07:02 +1000

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo mkdir /mnt
-echo '//192.168.2.1/Network /mnt cifs user=simon,pass=paradigm 0 0' | sudo tee -a /etc/fstabsmb_network_share
-
-
-
-
-
-sudo mount -t cifs //192.168.2.1/Network /mnt -o username=simon,password=paradigm
-sudo umount /mnt
